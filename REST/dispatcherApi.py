f4deac5f8aee8396e1e99c1bacfa18bfbbe78026
Bruno <bgarcia@lcc.uma.es>
2020-03-03 15:00:59 +0100

    Improve token handling logic

--- REST/dispatcherApi.py
+++ REST/dispatcherApi.py
@@ -1,9 +1,10 @@
 import json
-from typing import Dict
+from typing import Dict, Tuple
 from app.models import User
 from .restClient import RestClient
 from base64 import b64encode
-from Helper import Config
+from Helper import Config, Log
+from app import db
 
 
 class DispatcherApi(RestClient):
@@ -11,7 +12,16 @@ class DispatcherApi(RestClient):
         config = Config().Dispatcher
         super().__init__(config.Host, config.Port, "")
 
-    def Register(self, user: User):
+    @staticmethod
+    def basicAuthHeader(user: str, password: str) -> Dict:
+        encoded = b64encode(bytes(f'{user}:{password}'.encode('ascii')))
+        return {'Authorization': f'Basic {encoded.decode("ascii")}'}
+
+    @staticmethod
+    def bearerAuthHeader(token: str) -> Dict:
+        return {'Authorization': f'Bearer {token}'}
+
+    def Register(self, user: User) -> Dict:
         url = '/auth/register'
         data = {
             'username': user.username,
@@ -21,8 +31,33 @@ class DispatcherApi(RestClient):
         response = self.HttpPost(url, {'Content-Type': 'application/json'}, json.dumps(data))
         return self.ResponseToJson(response)
 
-    def GetToken(self, user: User):
+    def GetToken(self, user: User) -> Tuple[str, bool]:
+        """
+        Return a tuple (str, bool). The string contains the token OR the
+        error message, the boolean indicates success.
+        """
         url = '/auth/get_token'
-        encoded = b64encode(bytes(f'{user.username}:{user.password_hash}'.encode('ascii')))
-        response = self.HttpGet(url, extra_headers={'Authorization': f'Basic {encoded.decode("ascii")}'})
-        return self.ResponseToJson(response)
+        try:
+            response = self.HttpGet(url, extra_headers=self.basicAuthHeader(user.username, user.password_hash))
+            if response.status is not 200:
+                raise Exception(f"Status {response.status} ({response.reason})")
+            maybeToken = self.ResponseToJson(response)['result']
+            if "No user" in maybeToken or "not activated" in maybeToken:
+                raise Exception(maybeToken)
+            return maybeToken, True
+        except Exception as e:
+            message = f"Error while retrieving token: {e}"
+            Log.E(message)
+            return message, False
+
+    def RenewUserToken(self, user: User):
+        token, success = self.GetToken(user)
+        user.token = token if success else None
+        db.session.add(user)
+        db.session.commit()
+
+    def RunCampaign(self, experimentId: int, user_id: int) -> Dict:
+        user = User.query.get(user_id)
+        token = user.getCurrentDispatcherToken()
+        return {}
+
